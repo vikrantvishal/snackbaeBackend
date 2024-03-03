b76d25d337b06fd8f323c950a9a1a972d913b770
Tanmay <121565463+tanmay2809@users.noreply.github.com>
2024-03-03 16:56:55 +0530

    Update booking.js

--- Controllers/booking.js
+++ Controllers/booking.js
@@ -1,14 +1,12 @@
 const Bookings = require("../models/bookingmodel");
-const Guest = require("../models/guestmodel");
-const { transport } = require("../util/utils");
 const restaurantDetails = require("../models/restaurantDetails");
 //////////////////////
 // GET ALL BOOKINGS //
 //////////////////////
-const getBookings = async (req, res) => {
-  const bookings = await Bookings.find().populate("guest");
-  res.status(200).json(bookings);
-};
+// const getBookings = async (req, res) => {
+//   const bookings = await Bookings.find().populate("guest");
+//   res.status(200).json(bookings);
+// };
 //////////////////
 // SAVE BOOKING //
 //////////////////
@@ -27,7 +25,7 @@ const createBooking = async (req, res) => {
     }
 
     // Create a new booking
-    const booking = new Booking({
+    const booking = new Bookings({
       user: userId,
       restaurant: restaurantId,
       date,
@@ -59,101 +57,112 @@ const createBooking = async (req, res) => {
 // EDIT BOOKING //
 //////////////////
 const editBooking = async (req, res) => {
-  const id = req.params.id;
   try {
-    const editedBooking = await Bookings.findByIdAndUpdate(
-      {
-        _id: id,
-      },
-      {
-        date: req.body.date,
-        time: req.body.time,
-        amount: req.body.amount,
-        tables: req.body.tables,
-        message: req.body.message,
-      }
-    );
+    // Extract data from request body
+    const { date, time, numofpeople } = req.body;
 
-    // Find guest information
-    const getBooking = await Bookings.findById(id);
-    const guestID = getBooking.guest;
-    const foundGuest = await Guest.findById(guestID);
-    console.log(foundGuest);
-    // Send confirmation about updated reservation to guest
-    transport(foundGuest.email, req.body.date, id);
+    // Extract booking ID from URL parameters
+    const { bookingId } = req.params;
 
-    // Set 200 status and send response
-    res.status(200).send(editedBooking);
+    // Check if the booking exists
+    let booking = await Bookings.findById(bookingId);
+    if (!booking) {
+      return res.status(404).json({ error: "Booking not found" });
+    }
+
+    // Update the booking details
+    booking.date = date;
+    booking.time = time;
+    booking.numofpeople = numofpeople;
+
+    // Save the updated booking to the database
+    await booking.save();
+
+    // Return success response
+    res.status(200).json({ message: "Booking updated successfully", booking });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    console.error("Error updating booking:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
+
 
 ////////////////////
 // DELETE BOOKING //
 ////////////////////
 const deleteBooking = async (req, res) => {
-  const id = req.params.id;
-
   try {
-    // Find booking by ID and delete
-    await Bookings.findById(id).deleteOne();
-    res.status(200);
+    // Extract booking ID from URL parameters
+    const { bookingId } = req.params;
+
+    // Check if the booking exists
+    const booking = await Bookings.findById(bookingId);
+    if (!booking) {
+      return res.status(404).json({ error: "Booking not found" });
+    }
+
+    // Delete the booking from the database
+    await Bookings.findByIdAndDelete(bookingId);
+
+    // Return success response
+    res.status(200).json({ message: "Booking deleted successfully" });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    console.error("Error deleting booking:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
+
 
 //////////////////////
 // GET AVAILABILITY //
 //////////////////////
-const searchAvailability = async (req, res) => {
-  const { date, time, amount, tables } = req.body;
+// const searchAvailability = async (req, res) => {
+//   const { date, time, amount, tables } = req.body;
 
-  // Get all bookings from db
-  const allBookings = await Bookings.find().lean();
+//   // Get all bookings from db
+//   const allBookings = await Bookings.find().lean();
 
-  try {
-    // Get existing reservations from requested day
-    const requestedDate = allBookings.filter(
-      (booking) => booking.date === date
-    );
-    // If there are no reservations on requested date, confirm booking
-    if (requestedDate.length < 1) {
-      res.status(200).send(true);
-    } else {
-      /* If there are bookings on requested date,
-    check if there are any at the same time */
-      for (let i = 0; i < requestedDate.length; i++) {
-        // List of bookings on the same date and same time:
-        const sameDayAndTime = requestedDate.filter(
-          (booking) => booking.time === time
-        );
-        // If there are no bookings on requested time, confirm booking
-        if (sameDayAndTime.length < 1) {
-          res.status(200).send(true);
-        } else {
-          // Get list of bookings with same date & time:
-          for (let j = 0; j < sameDayAndTime.length; j++) {
-            // Get total amount of booked tables on same date & time
-            const bookedTables = sameDayAndTime.reduce(function (a, b) {
-              return a + b.tables;
-            }, 0);
-            // If 15 tables are already booked, decline booking request
-            if (bookedTables + tables > 15) {
-              res.status(200).send(false);
-            } // If there are tables available, confirm booking
-            else {
-              res.status(200).send(true);
-            }
-          }
-        }
-      }
-    }
-  } catch (error) {
-    res.status(400);
-  }
-};
+//   try {
+//     // Get existing reservations from requested day
+//     const requestedDate = allBookings.filter(
+//       (booking) => booking.date === date
+//     );
+//     // If there are no reservations on requested date, confirm booking
+//     if (requestedDate.length < 1) {
+//       res.status(200).send(true);
+//     } else {
+//       /* If there are bookings on requested date,
+//     check if there are any at the same time */
+//       for (let i = 0; i < requestedDate.length; i++) {
+//         // List of bookings on the same date and same time:
+//         const sameDayAndTime = requestedDate.filter(
+//           (booking) => booking.time === time
+//         );
+//         // If there are no bookings on requested time, confirm booking
+//         if (sameDayAndTime.length < 1) {
+//           res.status(200).send(true);
+//         } else {
+//           // Get list of bookings with same date & time:
+//           for (let j = 0; j < sameDayAndTime.length; j++) {
+//             // Get total amount of booked tables on same date & time
+//             const bookedTables = sameDayAndTime.reduce(function (a, b) {
+//               return a + b.tables;
+//             }, 0);
+//             // If 15 tables are already booked, decline booking request
+//             if (bookedTables + tables > 15) {
+//               res.status(200).send(false);
+//             } // If there are tables available, confirm booking
+//             else {
+//               res.status(200).send(true);
+//             }
+//           }
+//         }
+//       }
+//     }
+//   } catch (error) {
+//     res.status(400);
+//   }
+// };
 
 ////////////////////////
 // EMAIL CANCELLATION //
@@ -170,20 +179,11 @@ const emailCancellation = async (req, res) => {
   }
 };
 
-//////////////////////////////////////
-// CLEAR BOOKINGS IN DB for Cypress //
-//////////////////////////////////////
-const clearBookings = async (req, res) => {
-  await Bookings.deleteMany({});
-  res.sendStatus(200);
-};
 
 module.exports = {
   createBooking,
-  getBookings,
   editBooking,
   deleteBooking,
-  searchAvailability,
-  emailCancellation,
-  clearBookings,
+
+
 };
