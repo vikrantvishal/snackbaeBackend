2523dbef15c01afcb5af4c5c532d6c3a26c8950d
Tanmay <121565463+tanmay2809@users.noreply.github.com>
2024-03-03 14:09:39 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,34 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+ 
+contactNumber:{
+    type: Number,
     trim: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  additionalDetails: {
+},
+additionalDetails:{
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: "Profile",
   },
-  image: {
-    type: String,
-    required: true,
-  },
-  contactNumber: {
-    type: Number,
-    trim: true,
-  }
+
 });
 
 module.exports = mongoose.model("User", userSchema);
